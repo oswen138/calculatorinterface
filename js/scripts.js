3558bca31308e18dd0162d5affc8dd065a841ed5
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-12 11:30:20 -0700

    will make the initial files with javascripts for calculator
    co-author Ophelia Swen<ophelia.swen@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function add(number1, number2) { 
+  parseInt(number1);
+  parseInt(number2);
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+let result = add(number1, number2);
+alert(result);
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
+
+function subtract(number1, number2){
+  parseInt(number1);
+  parseInt(number2);
+  return number1-number2
+}
+
+function divide(number1,number2){
+parseInt(number1);
+parseInt(number2);
+return number1/number2
+}
