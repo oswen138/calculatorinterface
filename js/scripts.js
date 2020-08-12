3bb1226802076d551a5b48e03491ff202a7ba678
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-12 15:15:30 -0700

    will make all button and functions work
    for co-author Ophelia Swen <ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,28 @@ $(document).ready(function() {
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
+  });
   $("form#sub").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#sub1").val());
     const number2 = parseInt($("#sub2").val());
     const result = subtract(number1, number2);
     $("#output").text(result);
-    
-
-
-    })
   });
+    $("form#mulitply").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#mult1").val());
+      const number2 = parseInt($("#mult2").val());
+      const result = multiplyBy(number1, number2);
+      $("#output").text(result);
+    });
+      
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#div1").val());
+      const number2 = parseInt($("#div2").val());
+      const result = divide(number1, number2);
+      $("#output").text(result)
+    });
+  
 });
