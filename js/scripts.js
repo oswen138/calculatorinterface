43ca993e567095b157b1dd71bcc122b8dfe35135
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-12 15:02:42 -0700

    trying to make the subtraction button to work
    co-author Ophelia Swen<ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 function add(number1, number2) {
-  
   return number1 + number2;
-  
 }
 
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
-function multiplyBy()
-{
-  return number1 * number2
-}
+function multiplyBy(number1, number2) {
+  return number1 * number2;
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
+};
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
@@ -24,5 +21,15 @@ $(document).ready(function() {
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+    
+
+
+    })
   });
 });
